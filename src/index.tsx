535dabb914b41bd646b6d6738fd1215464e45a9e
Roman Mik <roman_mik@lineate.com>
2022-07-08 01:28:18 +0600

    feat: add linters

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {createRoot} from "react-dom/client";
+
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
   return <h1>This is my React app!</h1>;
-}
+};
 
-const container =  document.getElementById("cinema-web-app");
+const container = document.getElementById('cinema-web-app');
 const root = createRoot(container);
 
-root.render(<App/>);
+root.render(<App />);
