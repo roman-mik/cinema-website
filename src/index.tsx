cf4a1902d3ff1daf169a3a11fbb61f377c22b3ab
Roman Mik <roman_mik@lineate.com>
2022-07-08 00:21:24 +0600

    feat: add typescript

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {createRoot} from "react-dom/client";
+
+const App = () => {
+  return <h1>This is my React app!</h1>;
+}
+
+const container =  document.getElementById("cinema-web-app");
+const root = createRoot(container);
+
+root.render(<App/>);
